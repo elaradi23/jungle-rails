aaea78b6d800a449c923d3f6a57070b0d7118ff1
elaradi23 <ta.elaradi23@gmail.com>
2018-08-15 22:35:22 +0000

    Fixed missing admin security bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'], except: :index
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
